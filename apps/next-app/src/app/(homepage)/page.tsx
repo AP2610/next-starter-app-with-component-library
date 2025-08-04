2351ec0f91611d92d6e0b44b5d4bc1acde0f2e2f
Arjun Puri <arjun_261094@hotmail.com>
2025-08-05 00:22:37 +0200

    Fix tailwind package issues

--- apps/next-app/src/app/(homepage)/page.tsx
+++ apps/next-app/src/app/(homepage)/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-16 p-8 pb-20 sm:p-20">
-      <h1 className="font-raleway text-4xl font-bold">Get Started!</h1>
+      <h1 className="font-raleway text-4xl font-bold text-primary">
+        Get Started!
+      </h1>
     </div>
   );
 };
